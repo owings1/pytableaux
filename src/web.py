f28963fedb2a40a9febffda478e8210439e72d5b
Doug Owings <doug@lab5.co>
2020-04-29 22:34:24 -0700

    cleanup web

--- src/web.py
+++ src/web.py
@@ -19,6 +19,26 @@
 # pytableaux - Web Interface
 
 import examples, logic, json, os
+import importlib
+import os.path
+import cherrypy as server
+from jinja2 import Environment, PackageLoader
+
+# http://python-future.org/compatible_idioms.html#basestring
+from past.builtins import basestring
+
+default_host = '127.0.0.1'
+default_port = 8080
+envvar_host = 'PY_HOST'
+envvar_port = 'PY_PORT'
+index_filename = 'index.html'
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(current_dir, 'www', 'static')
+static_dir_doc = os.path.join(current_dir, '..', 'doc', '_build', 'html')
+view_path = 'www/views'
+
+jinja_env = Environment(loader=PackageLoader('logic', view_path))
 
 available_module_names = {
     'logics'    : ['cpl', 'cfol', 'k3', 'k3w', 'l3', 'lp', 'go', 'fde', 'k', 'd', 't', 's4'],
@@ -26,7 +46,6 @@ available_module_names = {
     'writers'   : ['html', 'ascii']
 }
 
-import importlib
 modules = {}
 for package in available_module_names:
     modules[package] = {}
@@ -38,77 +57,49 @@ for notation_name in modules['notations']:
     notation = modules['notations'][notation_name]
     notation_user_predicate_symbols[notation_name] = list(notation.symbol_sets['default'].chars('user_predicate'))
 
-import os.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
 global_config = {
     'global': {
-        'server.socket_host': os.environ['PY_HOST'] if 'PY_HOST' in os.environ else '127.0.0.1',
-        'server.socket_port': int(os.environ['PY_PORT']) if 'PY_PORT' in os.environ else 8080
+        'server.socket_host': os.environ[envvar_host] if envvar_host in os.environ else default_host,
+        'server.socket_port': int(os.environ[envvar_port]) if envvar_port in os.environ else default_port
     }
 }
 config = {
     '/static' : {
         'tools.staticdir.on'  : True,
-        'tools.staticdir.dir' : os.path.join(current_dir, 'www', 'static')
+        'tools.staticdir.dir' : static_dir
     }
 }
 
 config['/doc'] = {
     'tools.staticdir.on'    : True,
-    'tools.staticdir.dir'   : os.path.join(current_dir, '..', 'doc', '_build', 'html'),
-    'tools.staticdir.index' : 'index.html'
+    'tools.staticdir.dir'   : static_dir_doc,
+    'tools.staticdir.index' : index_filename
 }
 
-from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('logic', 'www/views'))
-
-import cherrypy as server
-
-class App:
+class App(object):
                 
     @server.expose
     def index(self, *args, **kw):
+
         App.fix_kw(kw)
+
+        print(args)
         print(kw)
-        data = {
-            'operators_list'     : logic.operators_list,
-            'logic_modules'      : available_module_names['logics'],
-            'logics'             : modules['logics'],
-            'writer_modules'     : available_module_names['writers'],
-            'writers'            : modules['writers'],
-            'notation_modules'   : available_module_names['notations'],
-            'notations'          : modules['notations'],
-            'form_data'          : kw,
-            'system_predicates'  : logic.system_predicates,
-            'quantifiers'        : logic.quantifiers_list,
-            'example_args_list'  : examples.args_list,
-            'app' : json.dumps({
-                'example_predicates'              : examples.test_pred_data,
-                'notation_user_predicate_symbols' : notation_user_predicate_symbols,
-                'num_predicate_symbols'           : logic.num_predicate_symbols,
-                'example_arguments' : {
-                    arg.title : {
-                        notation: {
-                            'premises'   : [modules['notations'][notation].write(premise) for premise in arg.premises],
-                            'conclusion' : modules['notations'][notation].write(arg.conclusion)
-                        }
-                        for notation in available_module_names['notations']
-                    }
-                    for arg in examples.arguments()
-                }
-            }, indent=2),
-            'version'           : logic.version,
-            'copyright'         : logic.copyright,
-            'source_href'       : logic.source_href
-        }
+
+        data = App.get_base_data(kw)
+
         vocabulary = logic.Vocabulary()
+
         view = 'argument'
+
         if len(kw) and ('errors' not in kw or not len(kw['errors'])):
+
             notation = modules['notations'][kw['notation']]
             writer = modules['writers'][kw['writer']].Writer()
             
             errors = {}
-            App.declare_user_predicates(kw, vocabulary, errors)
+            if 'user_predicate_arities[]' in kw:
+                App.declare_user_predicates(kw, vocabulary, errors)
             parser = notation.Parser(vocabulary)
 
             try:
@@ -165,7 +156,7 @@ class App:
         if 'errors' in kw:
             data['errors'] = kw['errors']
         return self.render(view, data)
-    
+
     @server.expose
     def parse(self, *args, **kw):
         notation = modules['notations'][kw['notation']]
@@ -179,70 +170,14 @@ class App:
         return ''
         
     def render(self, view, data={}):
-        return env.get_template(view + '.html').render(data)
-
-    # this is a work in progress
-    def prove(self, data):
-        input_data = {
-            'parser' : '',         # standard, polish
-            'output' : {
-                'formats'    : [], # list of logic names (fde, cfol, etc.)
-                'notation'   : '', # optional, default is parser.notation
-            },
-            'predicates' : {
-                'name' : {
-                    'index'     : 0,
-                    'subscript' : 0,
-                    'arity'     : 1
-                }
-            },
-            'argument' : {
-                'premises'   : [], # None or missing is allowed
-                'conclusion' : ''
-            },
-            'logics' : []
-        }
-        output_data = {
-            'argument' : {
-                'symbol_set' : {
-                    'premises'   : [],
-                    'conclusion' : ''
-                }
-            },
-            'predicate_strings' : {     # includes system predicates
-                'symbol_set' : {
-                    'name' : ''
-                }
-            },
-            'predicate_defs' : {            # includes system predicates
-                'name' : {
-                    'index'     : 0,
-                    'subscript' : 0,
-                    'arity'     : 1
-                }
-            },
-            'headers' : {
-                'format' : ''
-            },
-            'footers' : {
-                'format' : ''
-            },
-            'proofs' : {
-                'logicName' : {
-                    'valid' : None,
-                    'output' : {
-                        'format' : ''
-                    }
-                }
-            }
-        }
-        return output_data
+        return jinja_env.get_template(view + '.html').render(data)
 
     @staticmethod
     def fix_kw(kw):
         if len(kw):
             for param in kw:
                 if param.endswith('[]'):
+                    # http://python-future.org/compatible_idioms.html#basestring
                     if isinstance(kw[param], basestring):
                         kw[param] = [kw[param]]
 
@@ -260,9 +195,45 @@ class App:
                         errors['Predicate ' + str(i + 1)] = e
                 else:
                     errors['Predicate ' + str(i + 1)] = Exception('Name cannot be empty')
+
+    @staticmethod
+    def get_base_data(kw):
+        return {
+            'operators_list'     : logic.operators_list,
+            'logic_modules'      : available_module_names['logics'],
+            'logics'             : modules['logics'],
+            'writer_modules'     : available_module_names['writers'],
+            'writers'            : modules['writers'],
+            'notation_modules'   : available_module_names['notations'],
+            'notations'          : modules['notations'],
+            'form_data'          : kw,
+            'system_predicates'  : logic.system_predicates,
+            'quantifiers'        : logic.quantifiers_list,
+            'example_args_list'  : examples.args_list,
+            'app' : json.dumps({
+                'example_predicates'              : examples.test_pred_data,
+                'notation_user_predicate_symbols' : notation_user_predicate_symbols,
+                'num_predicate_symbols'           : logic.num_predicate_symbols,
+                'example_arguments' : {
+                    arg.title : {
+                        notation: {
+                            'premises'   : [modules['notations'][notation].write(premise) for premise in arg.premises],
+                            'conclusion' : modules['notations'][notation].write(arg.conclusion)
+                        }
+                        for notation in available_module_names['notations']
+                    }
+                    for arg in examples.arguments()
+                }
+            }, indent=2),
+            'version'           : logic.version,
+            'copyright'         : logic.copyright,
+            'source_href'       : logic.source_href
+        }
+        
         
 def main():
     server.config.update(global_config)
     server.quickstart(App(), '/', config)
 
-if  __name__ =='__main__':main()
+if  __name__ == '__main__':
+    main()
