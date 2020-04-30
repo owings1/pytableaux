740cf6125a114009cece43f419c077abba097324
Doug Owings <doug@lab5.co>
2020-04-29 20:15:14 -0700

    add proof writers tests

--- test/test_writers.py
+++ test/test_writers.py
@@ -2,6 +2,10 @@ import pytest
 
 from logic import *
 from notations import standard, polish
+from writers import ascii, asciiv, html
+import examples
+
+# Sentence Writers
 
 std = standard.Writer()
 pol = polish.Writer()
@@ -19,3 +23,37 @@ class TestPolish(object):
         s = atomic(0, 0)
         res = pol.write(s)
         assert res == 'a'
+
+# Proof writers
+
+asc = ascii.Writer()
+htm = html.Writer()
+asv = asciiv.Writer()
+
+def example_proof(logic, name, is_build=True):
+    arg = examples.argument(name)
+    proof = tableau(logic, arg)
+    if is_build:
+        proof.build()
+    return proof
+
+class TestAscii(object):
+
+    def test_write_std_fde_1(self):
+        proof = example_proof('fde', 'Addition')
+        res = asc.write(proof, standard)
+        # TODO: assert something
+
+class TestAsciiv(object):
+
+    def test_write_std_fde_1(self):
+        proof = example_proof('fde', 'Addition')
+        res = asv.write(proof, standard)
+        # TODO: assert something
+    
+class TestHtml(object):
+
+    def test_write_std_fde_1(self):
+        proof = example_proof('fde', 'Addition')
+        res = htm.write(proof, standard)
+        # TODO: assert something
