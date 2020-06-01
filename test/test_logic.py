82ed584624fa537be0f5c3901a4e558a1cf16bbd
Doug Owings <doug@dougowings.net>
2020-06-01 01:13:08 -0700

    additional tests

--- test/test_logic.py
+++ test/test_logic.py
@@ -21,6 +21,7 @@ import pytest
 
 import logic
 import examples
+import time
 
 def test_parse_standard():
     s = logic.parse('A & B', notation='standard')
@@ -136,6 +137,28 @@ class TestVocabulary(object):
         assert p.subscript == 0
         assert p.arity == 1
 
+    def test_vocab_copy_get_predicate(self):
+        v = logic.Vocabulary()
+        predicate = v.declare_predicate('MyPredicate', 0, 0, 1)
+        v2 = v.copy()
+        assert v2.get_predicate('MyPredicate') == predicate
+
+    def test_python2_cmp_self_0(self):
+        v = logic.Vocabulary()
+        predicate = v.declare_predicate('MyPredicate', 0, 0, 1)
+        assert predicate.__cmp__(predicate) == 0
+
+    def test_add_predicate_raises_non_predicate(self):
+        v = logic.Vocabulary()
+        with pytest.raises(logic.Vocabulary.PredicateError):
+            v.add_predicate('foo')
+
+    def test_add_predicate_raises_sys_predicate(self):
+        v = logic.Vocabulary()
+        pred = logic.get_system_predicate('Identity')
+        with pytest.raises(logic.Vocabulary.PredicateError):
+            v.add_predicate(pred)
+
     def test_declare_predicate_already_declared_sys(self):
         v = logic.Vocabulary()
         with pytest.raises(logic.Vocabulary.PredicateAlreadyDeclaredError):
@@ -157,6 +180,51 @@ class TestVocabulary(object):
         v = logic.Vocabulary()
         with pytest.raises(logic.Vocabulary.IndexTooLargeError):
             v.declare_predicate('MyPredicate', logic.num_predicate_symbols, 0, 1)
+
+    def test_declare_predicate_arity_non_int(self):
+        v = logic.Vocabulary()
+        with pytest.raises(logic.Vocabulary.PredicateArityError):
+            v.declare_predicate('MyPredicate', 0, 0, None)
+
+    def test_declare_predicate_arity_0_error(self):
+        v = logic.Vocabulary()
+        with pytest.raises(logic.Vocabulary.PredicateArityError):
+            v.declare_predicate('MyPredicate', 0, 0, 0)
+
+    def test_new_predicate_subscript_non_int(self):
+        v = logic.Vocabulary()
+        with pytest.raises(logic.Vocabulary.PredicateSubscriptError):
+            logic.Vocabulary.Predicate('MyPredicate', 0, None, 1)
+
+    def test_new_predicate_subscript_less_than_0_error(self):
+        with pytest.raises(logic.Vocabulary.PredicateSubscriptError):
+            logic.Vocabulary.Predicate('MyPredicate', 0, -1, 1)
+
+    def test_predicate_is_system_predicate_true(self):
+        assert logic.get_system_predicate('Identity').is_system_predicate()
+
+    def test_predicate_eq_true(self):
+        assert logic.get_system_predicate('Identity') == logic.get_system_predicate('Identity')
+
+    def test_predicate_system_less_than_user(self):
+        v = logic.Vocabulary()
+        predicate = v.declare_predicate('MyPredicate', 0, 0, 1)
+        assert logic.get_system_predicate('Identity') < predicate
+
+    def test_predicate_system_less_than_or_equal_to_user(self):
+        v = logic.Vocabulary()
+        predicate = v.declare_predicate('MyPredicate', 0, 0, 1)
+        assert logic.get_system_predicate('Identity') <= predicate
+
+    def test_predicate_user_greater_than_system(self):
+        v = logic.Vocabulary()
+        predicate = v.declare_predicate('MyPredicate', 0, 0, 1)
+        assert predicate > logic.get_system_predicate('Identity')
+
+    def test_predicate_user_greater_than_or_equal_to_system(self):
+        v = logic.Vocabulary()
+        predicate = v.declare_predicate('MyPredicate', 0, 0, 1)
+        assert predicate >= logic.get_system_predicate('Identity')
 
     def test_list_predicates_contains_identity(self):
         v = logic.Vocabulary()
@@ -285,12 +353,41 @@ class TestVocabulary(object):
         with pytest.raises(logic.Vocabulary.OperatorArityMismatchError):
             logic.operate('Negation', [s, s])
 
+    def test_constant_repr_contains_subscript(self):
+        c = logic.constant(0, 8)
+        res = str(c)
+        assert '8' in res
+
+    def test_base_sentence_not_implemented_various(self):
+        s = logic.Vocabulary.Sentence()
+        with pytest.raises(logic.NotImplementedError):
+            s.atomics()
+        with pytest.raises(logic.NotImplementedError):
+            s.predicates()
+        with pytest.raises(logic.NotImplementedError):
+            s.hash_tuple()
+
+    def test_atomic_less_than_predicated(self):
+        s1 = logic.atomic(0, 4)
+        s2 = examples.predicated()
+        assert s1 < s2
+        assert s1 <= s2
+        assert s2 > s1
+        assert s2 >= s1
+
+    def test_atomic_cmp_self_0_compat(self):
+        s = logic.atomic(0, 0)
+        assert s.__cmp__(s) == 0
+
 class TestTableauxSystem(object):
 
     def test_build_trunk_base_not_impl(self):
         proof = logic.tableau(None, None)
         with pytest.raises(logic.NotImplementedError):
             logic.TableauxSystem.build_trunk(proof, None)
+
+def mock_sleep_5ms():
+    time.sleep(0.005)
 
 class TestTableau(object):
 
@@ -310,6 +407,34 @@ class TestTableau(object):
         proof = logic.tableau('cpl', examples.argument('Addition'))
         res = proof.__repr__()
         assert 'finished' in res
+
+    def test_build_premature_max_steps(self):
+        proof = logic.tableau('cpl', examples.argument('Material Modus Ponens'))
+        proof.build(max_steps=1)
+        assert proof.is_premature
+
+    def test_step_raises_trunk_not_built_with_hacked_arg_prop(self):
+        proof = logic.tableau('cpl')
+        proof.argument = examples.argument('Addition')
+        with pytest.raises(logic.TableauxSystem.TrunkNotBuiltError):
+            proof.step()
+
+    def test_construct_sets_is_rank_optim_option(self):
+        proof = logic.tableau('cpl', is_rank_optim=False)
+        rule = proof.get_rule('Conjunction')
+        assert not proof.opts['is_rank_optim']
+        assert not rule.is_rank_optim
+
+    def test_timeout_1ms(self):
+        proof = logic.tableau('cpl', examples.argument('Addition'))
+        proof.step = mock_sleep_5ms
+        with pytest.raises(logic.TableauxSystem.ProofTimeoutError):
+            proof.build(timeout=1)
+
+    def test_finish_empty_sets_build_duration_none(self):
+        proof = logic.tableau(None, None)
+        proof.finish()
+        assert proof.stats['build_duration'] == None
 
 class TestBranch(object):
 
@@ -340,6 +465,48 @@ class TestBranch(object):
         b = logic.TableauxSystem.Branch()
         res = b.__repr__()
         assert 'closed' in res
+
+    def test_has_all_true_1(self):
+        b = logic.TableauxSystem.Branch()
+        s1 = logic.atomic(0, 0)
+        s2 = logic.atomic(1, 0)
+        s3 = logic.atomic(2, 0)
+        b.update([{'sentence': s1}, {'sentence': s2}, {'sentence': s3}])
+        check = [{'sentence': s1, 'sentence': s2}]
+        assert b.has_all(check)
+
+    def test_has_all_false_1(self):
+        b = logic.TableauxSystem.Branch()
+        s1 = logic.atomic(0, 0)
+        s2 = logic.atomic(1, 0)
+        s3 = logic.atomic(2, 0)
+        b.update([{'sentence': s1}, {'sentence': s3}])
+        check = [{'sentence': s1, 'sentence': s2}]
+        assert not b.has_all(check)
+
+    def test_atomics_1(self):
+        b = logic.TableauxSystem.Branch()
+        s1 = logic.atomic(0, 0)
+        s2 = logic.negate(logic.atomic(1, 0))
+        s3 = logic.atomic(1, 0)
+        b.update([{'sentence': s1}, {'sentence': s2}])
+        res = b.atomics()
+        assert s1 in res
+        assert s3 in res
+
+    def test_predicates(self):
+        b = logic.TableauxSystem.Branch()
+        s1 = examples.predicated()
+        s2 = logic.negate(logic.negate(s1))
+        b.add({'sentence': s2})
+        assert s1.predicate in b.predicates()
+
+    def test_make_model_raises_when_branch_closed(self):
+        proof = logic.tableau('cpl', examples.argument('Addition'))
+        proof.build()
+        b = list(proof.branches)[0]
+        with pytest.raises(logic.TableauxSystem.BranchClosedError):
+            b.make_model()
 
 class TestNode(object):
 
@@ -383,6 +550,16 @@ class TestBranchRule(object):
         with pytest.raises(logic.NotImplementedError):
             rule.applies_to_branch(None)
 
+    def test_target_has_branch_when_returns_true_mock(self):
+        class MockRule(logic.TableauxSystem.BranchRule):
+            def applies_to_branch(self, branch):
+                return True
+        proof = logic.tableau(None, None)
+        branch = proof.branch()
+        rule = MockRule(proof)
+        res = rule.applies()
+        assert res['branch'] == branch
+
 class TestClosureRule(object):
 
     def test_applies_to_branch_not_impl(self):
@@ -411,3 +588,39 @@ class TestNodeRule(object):
         rule = logic.TableauxSystem.NodeRule(logic.tableau(None, None))
         with pytest.raises(logic.NotImplementedError):
             rule.example()
+
+class TestSelectiveTrackingBranchRule(object):
+
+    def test_not_impl_various(self):
+        rule = logic.TableauxSystem.SelectiveTrackingBranchRule(logic.tableau(None, None))
+        with pytest.raises(logic.NotImplementedError):
+            rule.get_candidate_targets_for_branch(None)
+        with pytest.raises(logic.NotImplementedError):
+            rule.apply_to_target(None)
+
+class TestModel(object):
+
+    def test_not_impl_various(self):
+        model = logic.Model()
+        with pytest.raises(logic.NotImplementedError):
+            model.read_branch(None)
+        with pytest.raises(logic.NotImplementedError):
+            model.truth_function(None, None)
+        with pytest.raises(logic.NotImplementedError):
+            model.value_of_opaque(None)
+        with pytest.raises(logic.NotImplementedError):
+            model.value_of_predicated(None)
+        with pytest.raises(logic.NotImplementedError):
+            s = logic.negate(logic.atomic(0, 0))
+            model.value_of_operated(s)
+        with pytest.raises(logic.NotImplementedError):
+            model.value_of_quantified(None)
+        with pytest.raises(logic.NotImplementedError):
+            model.is_countermodel_to(None)
+        with pytest.raises(logic.NotImplementedError):
+            model.value_of_atomic(None)
+
+    def test_get_data_empty(self):
+        model = logic.Model()
+        res = model.get_data()
+        assert len(res) == 0
