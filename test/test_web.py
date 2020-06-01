ba79a7332ddd3ac421f1e2ac5374723a83df1bf9
Doug Owings <doug@dougowings.net>
2020-06-01 02:44:28 -0700

    web tests

--- test/test_web.py
+++ test/test_web.py
@@ -20,7 +20,9 @@
 import pytest
 
 import web
+import logic
 import cherrypy
+import json
 from cherrypy.test import helper
 
 def test_instantiate():
@@ -28,6 +30,16 @@ def test_instantiate():
 
 # see https://docs.cherrypy.org/en/latest/tutorials.html#tutorial-12-using-pytest-and-code-coverage
 class AppTest(helper.CPWebCase):
+
+    def post_json(self, page, data):
+        body = json.dumps(data)
+        headers = [
+            ('Content-type', 'application/json'),
+            ('Content-Length', str(len(body)))
+        ]
+        res_raw = self.getPage(page, headers, 'POST', body)
+        res_list = list(res_raw)
+        return json.loads(res_list[2])
 
     @staticmethod
     def setup_server():
@@ -37,13 +49,172 @@ class AppTest(helper.CPWebCase):
         self.getPage('/')
         self.assertStatus('200 OK')
 
-    def test_index_trivial_invalid_polish_ascii_cpl(self):
-        kw = {
-            'input_notation'  : 'polish',
-            'output_notation' : 'polish',
-            'format'          : 'ascii',
-            'logic'           : 'cpl',
-            'conclusion'      : 'a'
-        }
+    def test_index_ok_1(self):
         app = web.App()
+        kw = {
+            'api-json': json.dumps({
+                'argument': {
+                    'conclusion': 'a'
+                },
+                'logic': 'cpl'
+            })
+        }
         res = app.index(**kw)
+        assert 'html-writer-proof' in res
+
+    def test_index_fail_bad_api_data_1(self):
+        app = web.App()
+        kw = {
+            'api-json': 'badjson'
+        }
+        res = app.index(**kw)
+        assert 'correct the following errors' in res
+
+    def test_index_fail_bad_arg_1(self):
+        app = web.App()
+        kw = {
+            'api-json': json.dumps({
+                'argument': {
+                    'conclusion' : 'asdf'
+                },
+                'logic': 'cpl'
+            })
+        }
+        res = app.index(**kw)
+        assert 'correct the following errors' in res
+
+    def test_api_parse_1(self):
+        app = web.App()
+        body = {
+            'notation': 'polish',
+            'input'   : 'Fm',
+            'predicates' : [
+               {
+                  'name'      : 'is F',
+                  'index'     : 0,
+                  'subscript' : 0,
+                  'arity'     : 1
+               }
+            ]
+        }
+        res = app.api_parse(body)
+        assert res['type'] == 'PredicatedSentence'
+
+    def test_api_parse_2(self):
+        app = web.App()
+        body = {
+            'input': 'a'
+        }
+        res = app.api_parse(body)
+        assert res['type'] == 'AtomicSentence'
+
+    def test_api_parse_invalid_notation(self):
+        app = web.App()
+        body = {
+            'notation': 'nonexistent',
+            'input': 'a'
+        }
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_parse(body)
+            assert 'Notation' in exc_info.value.errors
+
+    def test_api_parse_missing_input(self):
+        app = web.App()
+        body = {}
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_parse(body)
+            assert 'Sentence' in exc_info.value.errors
+
+    def test_api_parse_bad_predicate_data(self):
+        app = web.App()
+        body = {
+            'input': 'a',
+            'predicates': [
+                {'arity': 'asdf'}
+            ]
+        }
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_parse(body)
+            assert 'Predicate 1' in exc_info.value.errors
+
+    def test_api_prove_cpl_addition(self):
+        app = web.App()
+        body = {
+            'argument': {
+                'premises': ['a'],
+                'conclusion': 'Aab'
+            },
+            'logic': 'cpl'
+        }
+        res = app.api_prove(body)
+        assert res['tableau']['valid']
+
+    def test_api_errors_various(self):
+        app = web.App()
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'logic': 'bunky'})
+            assert 'Logic' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'output': {'symbol_set': 'bunky'}})
+            assert 'Symbol Set' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'output': {'notation': 'bunky'}})
+            assert 'Output notation' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'output': {'format': 'bunky'}})
+            assert 'Output format' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'max_steps': 'bunky'})
+            assert 'Max steps' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'argument': {'notation': 'bunky'}})
+            assert 'Notation' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'argument': {'predicates': [{'arity': 'bunky'}]}})
+            assert 'Predicate 1' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'argument': {'premises': ['bunky']}})
+            assert 'Premise 1' in exc_info.value.errors
+        with pytest.raises(web.RequestDataError) as exc_info:
+            app.api_prove({'argument': {'conclusion': 'bunky'}})
+            assert 'Conclusion' in exc_info.value.errors
+
+    def test_post_api_prove_1(self):
+        body = {
+            'argument': {
+                'premises': ['a'],
+                'conclusion': 'Aab'
+            },
+            'logic': 'cpl'
+        }
+        res = self.post_json('/api/prove', body)
+        assert res['message'] == 'OK'
+
+    def test_post_api_prove_400_1(self):
+        body = {
+            'argument': {
+                'conclusion': 'bunky'
+            },
+            'logic': 'cpl'
+        }
+        res = self.post_json('/api/prove', body)
+        assert res['status'] == 400
+
+    def test_post_api_parse_1(self):
+        body = {
+            'input': 'a'
+        }
+        res = self.post_json('/api/parse', body)
+        assert res['result']['type'] == 'AtomicSentence'
+
+    def test_post_api_404_1(self):
+        body = {}
+        res = self.post_json('/api/bunky', body)
+        assert res['status'] == 404
+
+    def test_fix_form_data_braces_1(self):
+        form_data = {
+            'test[]': 'a'
+        }
+        res = web.fix_form_data(form_data)
+        assert isinstance(res['test[]'], list)
