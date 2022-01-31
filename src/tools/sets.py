c81bae3b422b9c1aaa081aa0a229dee9848619f0
Doug Owings <doug@dougowings.net>
2022-01-30 19:16:16 -0800

    set api cover

--- src/tools/sets.py
+++ src/tools/sets.py
@@ -1,13 +1,21 @@
 from __future__ import annotations
 
-__all__ = 'SetApi', 'MutableSetApi', 'setf', 'setm', 'EMPTY_SET'
+__all__ = (
+    'SetApi',
+    'MutableSetApi',
+    'setf',
+    'setm',
+    'SetCover',
+    'EMPTY_SET',
+)
 
-from tools.abcs import Copyable, T, VT
+from errors import instcheck
+from tools.abcs import Copyable, VT
 from tools.decorators import abstract, final, overload, operd
 
 from collections.abc import Set, MutableSet
 import operator as opr
-from typing import Iterable
+from typing import Iterable, TypeVar
 
 EMPTY = ()
 
@@ -17,13 +25,13 @@ class SetApi(Set[VT], Copyable):
     __slots__ = EMPTY
 
     @overload
-    def __or__(self:T, other) -> T: ...
+    def __or__(self:SetApiT, other) -> SetApiT: ...
     @overload
-    def __and__(self:T, other) -> T: ...
+    def __and__(self:SetApiT, other) -> SetApiT: ...
     @overload
-    def __sub__(self:T, other) -> T: ...
+    def __sub__(self:SetApiT, other) -> SetApiT: ...
     @overload
-    def __xor__(self:T, other) -> T: ...
+    def __xor__(self:SetApiT, other) -> SetApiT: ...
 
     __or__  = Set.__or__
     __and__ = Set.__and__
@@ -38,19 +46,19 @@ class SetApi(Set[VT], Copyable):
     @overload
     def issuperset(self, other: Iterable) -> bool: ...
     @overload
-    def union(self:T, *others: Iterable) -> T: ...
+    def union(self:SetApiT, *others: Iterable) -> SetApiT: ...
     @overload
-    def intersection(self:T, *others: Iterable) -> T: ...
+    def intersection(self:SetApiT, *others: Iterable) -> SetApiT: ...
     @overload
-    def difference(self:T, *others: Iterable) -> T: ...
+    def difference(self:SetApiT, *others: Iterable) -> SetApiT: ...
     @overload
-    def symmetric_difference(self:T, other: Iterable) -> T: ...
+    def symmetric_difference(self:SetApiT, other: Iterable) -> SetApiT: ...
 
-    issubset = app(opr.le, set.issubset)
-    issuperset = app(opr.ge, set.issuperset)
-    union = red(opr.or_, set.union)
+    issubset     = app(opr.le,   set.issubset)
+    issuperset   = app(opr.ge,   set.issuperset)
+    union        = red(opr.or_,  set.union)
     intersection = red(opr.and_, set.intersection)
-    difference = red(opr.sub, set.difference)
+    difference   = red(opr.sub,  set.difference)
     symmetric_difference = app(opr.xor, set.symmetric_difference)
 
     del(red, app)
@@ -59,7 +67,7 @@ class SetApi(Set[VT], Copyable):
         return self._from_iterable(self)
 
     @classmethod
-    def _from_iterable(cls: type[T], it: Iterable) -> T:
+    def _from_iterable(cls: type[SetApiT], it: Iterable) -> SetApiT:
         return cls(it)
 
 class MutableSetApi(MutableSet[VT], SetApi[VT]):
@@ -79,9 +87,12 @@ class MutableSetApi(MutableSet[VT], SetApi[VT]):
     def symmetric_difference_update(self, other: Iterable): ...
 
     update = rep(opr.ior, set.update)
+
     intersection_update = rep(opr.iand, set.intersection_update)
-    difference_update = rep(opr.isub, set.difference_update)
-    symmetric_difference_update = operd.apply(opr.ixor,
+    difference_update   = rep(opr.isub, set.difference_update)
+
+    symmetric_difference_update = operd.apply(
+        opr.ixor,
         set.symmetric_difference_update
     )
 
@@ -104,8 +115,22 @@ class setm(MutableSetApi[VT], set[VT]):
     add     = set.add
     discard = set.discard
 
-EMPTY_SET = setf()
+class SetCover(SetApi[VT]):
+    'SetApi cover.'
+    __slots__ = '__contains__', '__iter__', '__len__'
 
-del(opr, operd, Copyable, Set, MutableSet, EMPTY)
+    def __init__(self, set_: Set[VT], /):
+        instcheck(set_, Set)
+        self.__contains__ = set_.__contains__
+        self.__len__      = set_.__len__
+        self.__iter__     = set_.__iter__
 
+    @classmethod
+    def _from_iterable(cls, it: Iterable[VT]):
+        return cls(frozenset(it))
+
+del(opr, operd, Copyable, EMPTY)
 del(abstract, final, overload)
+
+SetApiT = TypeVar('SetApiT', bound = SetApi)
+EMPTY_SET = setf()
