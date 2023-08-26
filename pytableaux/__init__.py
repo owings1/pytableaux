9368c39eb9fd5a72c736a3c744a014724e0a2940
Doug Owings <doug@dougowings.net>
2023-08-26 02:39:34 -0700

    bump version

--- pytableaux/__init__.py
+++ pytableaux/__init__.py
@@ -25,7 +25,7 @@ import os.path
 import typing
 from dataclasses import dataclass
 
-__version__ = 2, 2, 6, 'final'
+__version__ = 2, 2, 7, 'dev'
 'Version tuple (major, minor, patch, release).'
 
 __year__ = 2023
