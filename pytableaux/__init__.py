67832c4a6d594ca59f35e5072ed3ab9efb930c81
Doug Owings <doug@dougowings.net>
2023-08-30 06:21:30 -0700

    v2.2.8-final

--- pytableaux/__init__.py
+++ pytableaux/__init__.py
@@ -25,7 +25,7 @@ import os.path
 import typing
 from dataclasses import dataclass
 
-__version__ = 2, 2, 8, 'dev'
+__version__ = 2, 2, 8, 'final'
 'Version tuple (major, minor, patch, release).'
 
 __year__ = 2023
